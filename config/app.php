07b7348d5aea9bd12924fc9ea91003c332cdc163
Davin <davinvin744@gmail.com>
2024-08-02 04:43:48 +0700

    fix(url): change url

--- config/app.php
+++ config/app.php
@@ -52,7 +52,7 @@ return [
     |
     */
 
-    'url' => env('APP_URL', 'http://localhost'),
+    'url' => env('APP_URL', 'https://zullkit-backend-87125781cc8c.herokuapp.com/'),
 
     'asset_url' => env('ASSET_URL', null),
 
